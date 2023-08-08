9d4b20049d18fc417f3f397b8564d14e4fd5c2b8
TuannQuangg <baonguyen130402@gmail.com>
2023-08-08 23:36:13 +0700

    This is my ClapClap

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+$('#tes').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
